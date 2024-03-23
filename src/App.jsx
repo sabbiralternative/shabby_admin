165c54758b19b27863b3837f2f7be983ae5cbd9e
sabbirHossain <sabbirshnt@gmail.com>
2024-03-23 14:26:53 +0600

    disable devtool

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Footer from "./components/Footer/Footer";
@@ -6,11 +6,14 @@ import { useEffect } from "react";
 import Success from "./components/Notification/Success";
 import UseContextState from "./hooks/UseContextState";
 import { config } from "./utils/config";
+import disableDevtool from "disable-devtool";
 
 const App = () => {
-  const location = useLocation()
+  const location = useLocation();
   const { loginSuccess, setLoginSuccess } = UseContextState();
   const pageTitle = config?.result?.settings?.siteTitle;
+  const disabledDevtool = config?.result?.settings?.disabledDevtool;
+  const navigate = useNavigate();
   useEffect(() => {
     document.title = pageTitle;
   }, [pageTitle]);
@@ -25,13 +28,27 @@ const App = () => {
     }
   }, [location]);
 
+  /* Disabled devtool based on settings */
+  useEffect(() => {
+    if (disabledDevtool) {
+      disableDevtool({
+        ondevtoolopen: (type) => {
+          const info = "devtool opened!; type =" + type;
+          if (info) {
+            localStorage.clear();
+            navigate("/login");
+          }
+        },
+      });
+    }
+  }, [navigate, disabledDevtool]);
+
   return (
     <div>
       {loginSuccess && (
         <Success message={loginSuccess} setMessage={setLoginSuccess} />
       )}
-      <Navbar
-      />
+      <Navbar />
       <Sidebar />
       <div data-v-b00d14ae="" className="main-content">
         <Outlet />
