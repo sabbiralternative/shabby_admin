0f71ea91968afbe0d6a3067de450ebc098750f93
sabbirHossain <sabbirshnt@gmail.com>
2026-07-15 11:51:09 +0600

    done

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,15 +1,13 @@
-import OurCasino from "./OurCasino";
+// import OurCasino from "./OurCasino";
 import Balance from "./Balance";
 
-
 const HomePage = () => {
-
   return (
     <div data-v-b00d14ae="" className="page-content">
       <div data-v-b00d14ae="">
         <div data-v-b00d14ae="" className="p-1">
           <Balance />
-          <OurCasino />
+          {/* <OurCasino /> */}
         </div>
       </div>
     </div>
